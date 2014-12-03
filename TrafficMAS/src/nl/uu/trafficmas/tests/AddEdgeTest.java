a216ba333bda51ce307a7e6cb90ebaff16e52cc8
Jetze Baumfalk <j.t.baumfalk@gmail.com>
2014-12-03 14:52:06 +0100

    Seperated tests

---
+++ TrafficMAS/src/nl/uu/trafficmas/tests/AddEdgeTest.java
@@ -0,0 +1,31 @@
+package nl.uu.trafficmas.tests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import nl.uu.trafficmas.roadnetwork.Edge;
+import nl.uu.trafficmas.roadnetwork.Node;
+import nl.uu.trafficmas.roadnetwork.Road;
+import nl.uu.trafficmas.roadnetwork.RoadNetwork;
+
+import org.junit.Test;
+
+public class AddEdgeTest {
+
+	@Test
+	public void addEdge() {
+		RoadNetwork rn = new RoadNetwork();
+		Node n = new Node();
+		Road r = new Road();
+		rn.addNode(n);
+		Edge edge = new Edge(n,n,r);
+		rn.addEdge(edge);
+		assertEquals(rn.getEdges().length, 1);
+		assertEquals(rn.getEdges()[0], edge);
+		
+		Node n2 = new Node();
+		rn.addEdge(n, n2, r);	
+		assertEquals(rn.getEdges().length, 2);
+		assertNotEquals(rn.getEdges()[1], edge);
+	}
+
+}
