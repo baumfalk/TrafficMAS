0bf721c94c3ca6ec598c0c6a6d5413885bafac62
Jetze Baumfalk <j.t.baumfalk@gmail.com>
2014-12-05 12:29:32 +0100

    Can read xml

--- TrafficMAS/src/LoadXML.java
+++ TrafficMAS/src/LoadXML.java
@@ -1,7 +1,41 @@
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Iterator;
+
+import javax.xml.namespace.QName;
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.events.Attribute;
+import javax.xml.stream.events.StartElement;
+import javax.xml.stream.events.XMLEvent;
+
 public class LoadXML {
 
 	public static void main(String[] args) {
-		
+		try{
+		  // First, create a new XMLInputFactory
+	      XMLInputFactory inputFactory = XMLInputFactory.newInstance();
+	      // Setup a new eventReader
+	      InputStream in = new FileInputStream("sim/hello.mas.xml");
+	      XMLEventReader eventReader = inputFactory.createXMLEventReader(in);
+	      while (eventReader.hasNext()) {
+	    	XMLEvent e = eventReader.nextEvent();
+	    	if(e.isStartElement()) {
+	    		StartElement se = e.asStartElement();
+	    		System.out.println(se.getName());
+	    		Iterator<Attribute> it = se.getAttributes();
+	    		while(it.hasNext()) {
+	    			Attribute a = it.next();
+	    			System.out.println("\t"+a.getValue());	    			
+	    		}
+	    	}
+	      
+	      }
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
 	}
 
 }
