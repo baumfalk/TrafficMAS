85b90db11bbf81e65fabd1a1160297dd5d92c70f
Jetze Baumfalk <j.t.baumfalk@gmail.com>
2014-12-10 15:04:08 +0100

    more tests

---
+++ TrafficMAS/src/nl/uu/trafficmas/tests/XMLEdgeTest.java
@@ -0,0 +1,41 @@
+package nl.uu.trafficmas.tests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import nl.uu.trafficmas.TrafficModelXML;
+import nl.uu.trafficmas.roadnetwork.Edge;
+import nl.uu.trafficmas.roadnetwork.LaneType;
+import nl.uu.trafficmas.roadnetwork.Node;
+
+import org.junit.Test;
+
+public class XMLEdgeTest {
+
+	@Test
+	public void test() {
+		//    <edge from="A28H0" id="A28Tot350" to="A28H350" numLanes="2" priority="2" />
+
+		TrafficModelXML model = new TrafficModelXML();
+		HashMap<String, Node> nodes = model.extractNodes("tests/", "NodeTest.xml");
+		ArrayList<Edge> edges = model.extractEdges("tests/", "EdgeTest.xml", nodes);
+		assertNotNull(edges);
+		assertEquals(edges.size(),1);
+		assertEquals(edges.get(0).getFromNode().name,"A28H0");
+		assertEquals(edges.get(0).getToNode().name,"A28H350");
+		assertEquals(edges.get(0).getRoad().priority,2);
+		assertEquals(edges.get(0).getRoad().getLanes().length,2);
+
+		assertEquals(edges.get(0).getRoad().getLanes()[0].laneType,LaneType.Normal);
+		assertEquals(edges.get(0).getRoad().getLanes()[1].laneType,LaneType.Normal);
+		double l = Node.nodeDistance(edges.get(0).getFromNode(), edges.get(0).getToNode());
+		assertEquals(edges.get(0).getRoad().length, l,0);
+		
+		
+	}
+
+}
--- TrafficMAS/src/nl/uu/trafficmas/tests/XMLNodeTest.java
+++ TrafficMAS/src/nl/uu/trafficmas/tests/XMLNodeTest.java
@@ -11,14 +11,12 @@ import org.junit.Test;
 
 public class XMLNodeTest {
 
-	private HashMap<String, Node> nodes;
-
 	@Test
 	public void test() {
 		TrafficModelXML model = new TrafficModelXML();
 //		<node id="A28H0" x="-350.0" y="0.0" />
 //	    <node id="A28H350" x="0.0" y="0.0" /> 
-		nodes = model.extractNodes("tests/", "NodeTest.xml");
+		HashMap<String, Node> nodes = model.extractNodes("tests/", "NodeTest.xml");
 		
 		assertEquals(nodes.size(),2); 
 		assertEquals(nodes.get("A28H0"),new Node("A28H0",-350,0));
