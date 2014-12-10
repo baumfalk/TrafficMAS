bc3c2797949c39b99f8230952036a1668d46690a
Jetze Baumfalk <j.t.baumfalk@gmail.com>
2014-12-10 14:39:21 +0100

    more tests

--- TrafficMAS/src/nl/uu/trafficmas/tests/ExtractFromXMLTest.java
+++ TrafficMAS/src/nl/uu/trafficmas/tests/ExtractFromXMLTest.java
@@ -15,10 +15,21 @@ public class ExtractFromXMLTest {
 	@Test
 	public void test() {
 
-		ArrayList<ArrayList<KeyValue<String, String>>> result = SimpleXMLReader.extractFromXML("", "ExtractFromXMLTest.xml", "test1");
+		ArrayList<ArrayList<KeyValue<String, String>>> result = SimpleXMLReader.extractFromXML("tests/", "ExtractFromXMLTest.xml", "test1");
 		assertNotNull("Result shouldn't be null", result);
 		assertEquals(result.size(),1);
-		fail("Not yet implemented");
+		assertEquals(result.get(0).size(),2);
+		int i =0;
+		for (KeyValue<String, String> attribute : result.get(0)) {
+			if(i==0) {
+				assertEquals(attribute.key,"aap");
+				assertEquals(attribute.value,"noot");
+			}
+			else if(i==1) {
+				assertEquals(attribute.key,"mies");
+				assertEquals(attribute.value,"schaap");
+			}
+			i++;
+		}
 	}
-
 }
