d2928fc745866eef5e4d28d0f2ff44c8f177e6f4
Maxic <barendez@gmail.com>
2014-12-10 09:45:34 +0100

    Added new scenario + some changes

--- TrafficMAS/src/nl/uu/trafficmas/agent/Agent.java
+++ TrafficMAS/src/nl/uu/trafficmas/agent/Agent.java
@@ -2,14 +2,34 @@ package nl.uu.trafficmas.agent;
 
 import java.util.ArrayList;
 
+import de.tudresden.sumo.cmd.Vehicle;
+import de.tudresden.sumo.util.SumoCommand;
+import de.tudresden.ws.container.SumoStringList;
 import nl.uu.trafficmas.organisation.Sanction;
 import nl.uu.trafficmas.roadnetwork.Node;
 
 public abstract class Agent extends AgentPhysical {
+	private String agentID;
 	private Node goalNode;
 	private int goalArrivalTime;
 	
 	private int expectedArrivalTime;
 	private ArrayList<Sanction> currentSanctionList;
 	public abstract double utility(int arrivalTime, ArrayList<Sanction> sanctionList);
+	
+	// Change speed by decelerating to goalVelocity.
+	public SumoCommand changeVelocity(float goalVelocity){
+		return Vehicle.setSpeed(agentID, goalVelocity);
+	}
+	
+	// Change Lane for a certain duration of time.
+	public SumoCommand changeLane(byte laneIndex, int duration){
+		return Vehicle.changeLane(agentID, laneIndex, duration);
+	}
+	
+	// Calculate new route according to goalRoadID
+	public SumoCommand changeRoad(int goalRoadID){
+		SumoStringList edgeList = new SumoStringList();
+		return Vehicle.setRoute(agentID, edgeList);
+	}
 }
---
+++ TrafficMAS/src/nl/uu/trafficmas/tests/RemoveEdgeTest.java
@@ -0,0 +1,26 @@
+package nl.uu.trafficmas.tests;
+
+import static org.junit.Assert.*;
+import nl.uu.trafficmas.roadnetwork.Edge;
+import nl.uu.trafficmas.roadnetwork.Node;
+import nl.uu.trafficmas.roadnetwork.Road;
+import nl.uu.trafficmas.roadnetwork.RoadNetwork;
+
+import org.junit.Test;
+
+public class RemoveEdgeTest {
+
+	@Test
+	public void test() {
+		RoadNetwork rn = new RoadNetwork();
+		Node n = new Node();
+		Road r = new Road();
+		rn.addNode(n);
+		Edge edge = new Edge(n,n,r);
+		rn.addEdge(edge);
+		assertEquals(rn.getEdges().length, 1);
+		rn.removeEdge(edge);
+		assertEquals(rn.getEdges().length, 0);
+	}
+
+}
