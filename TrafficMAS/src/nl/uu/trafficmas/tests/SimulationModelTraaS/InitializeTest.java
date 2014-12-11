dd1f2f2f95b76548600eb92155ad290d2638ad68
Maxic <barendez@gmail.com>
2014-12-11 11:39:14 +0100

    Test for SimulationModelTraaS

---
+++ TrafficMAS/src/nl/uu/trafficmas/tests/SimulationModelTraaS/InitializeTest.java
@@ -0,0 +1,30 @@
+package nl.uu.trafficmas.tests.SimulationModelTraaS;
+
+import static org.junit.Assert.*;
+import it.polito.appeal.traci.SumoTraciConnection;
+import nl.uu.trafficmas.SimulationModelTraaS;
+
+import org.junit.Test;
+
+import de.tudresden.sumo.cmd.Route;
+import de.tudresden.sumo.cmd.Simulation;
+
+public class InitializeTest {
+
+	@Test
+	public void initialize() {
+
+		
+		SumoTraciConnection conn = SimulationModelTraaS.initialize("sumo", "./tests/ConfigTest.xml");
+		try {
+            int simtime = (int) conn.do_job_get(Simulation.getCurrentTime());
+			conn.do_timestep();
+			assertEquals(1000, simtime);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
---
+++ TrafficMAS/src/nl/uu/trafficmas/tests/SimulationModelTraaS/InitializeWithOptionTest.java
@@ -0,0 +1,14 @@
+package nl.uu.trafficmas.tests.SimulationModelTraaS;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class InitializeWithOptionTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
