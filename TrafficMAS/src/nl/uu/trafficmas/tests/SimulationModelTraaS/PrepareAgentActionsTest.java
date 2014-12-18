532ac566b5b1a684336e20270d37a25e89f5a9ac
Maxic <barendez@gmail.com>
2014-12-18 12:40:21 +0100

    :eyes: Small changes to tests

--- TrafficMAS/src/nl/uu/trafficmas/tests/SimulationModelTraaS/PrepareAgentActionsTest.java
+++ TrafficMAS/src/nl/uu/trafficmas/tests/SimulationModelTraaS/PrepareAgentActionsTest.java
@@ -22,14 +22,15 @@ import de.tudresden.sumo.cmd.Vehicle;
 
 public class PrepareAgentActionsTest {
 
-	//@Test
+	@Test
 	public void changeLane() {
+		double agent1Speed = 10.0;
 		HashMap<String, String> options = new HashMap<String, String>();
-		options.put("e", "60");
+		options.put("e", "35");
 		options.put("start", "1");
 		options.put("quit-on-end", "1");
 		
-		SumoTraciConnection conn = SimulationModelTraaS.initializeWithOption(options,"sumo-gui", "./tests/ConfigTest.xml");
+		SumoTraciConnection conn = SimulationModelTraaS.initializeWithOption(options,"sumo", "./tests/ConfigTest.xml");
 		RoadNetwork rn = DataModelXML.instantiateRoadNetwork("./tests/", "NodeTest.xml", "EdgeTest.xml");
 		ArrayList<Pair<Agent, Integer>> agentPairList = new ArrayList<Pair<Agent, Integer>>();
 		Agent a1 = new NormalAgent("agent1", rn.getNodes()[1], 6000, 70.0);
@@ -44,10 +45,13 @@ public class PrepareAgentActionsTest {
 		HashMap<String, Agent> currentAgentMap = SimulationModelTraaS.updateCurrentAgentMap(completeAgentMap, new HashMap<String, Agent>(), conn);
 
 		try {
+			
 			// Disable automatic SUMO overtaking, but enable automatic right drive changes.
 			conn.do_job_set(Vehicle.setLaneChangeMode(a2.agentID, 0b0001000000));
 			conn.do_job_set(Vehicle.setLaneChangeMode(a1.agentID, 0b0001000000));
-			conn.do_job_set(Vehicle.setSpeed(a1.agentID, 10.0));
+			
+			// Reduce velocity of front car, so a2 can execute an overtaking action.
+			conn.do_job_set(Vehicle.setSpeed(a1.agentID, agent1Speed));
 
 			int i = 0;
 			// Let some time pass so both agents are spawned and moving
@@ -56,23 +60,24 @@ public class PrepareAgentActionsTest {
 				i++;
 				currentAgentMap = SimulationModelTraaS.updateCurrentAgentMap(completeAgentMap, currentAgentMap, conn);
 			}
-			HashMap<String, AgentPhysical> agentPhysMap =  SimulationModelTraaS.updateAgentsPhys(rn, currentAgentMap, conn);
-			HashMap<String, AgentPhysical> leadingVehicleMap = SimulationModelTraaS.getLeadingVehicles(agentPhysMap, conn);
+			SimulationModelTraaS.updateAgentsPhys(rn, currentAgentMap, conn);
 			HashMap<String, AgentAction> actions = new HashMap<String, AgentAction>();
 			
-			// Check for every action
-			// ChangeLane
+			// Testing action ChangeLane
 			AgentAction changeLaneAction = AgentAction.ChangeLane;
 			actions.put(a2.agentID, changeLaneAction);
-			
 			SimulationModelTraaS.prepareAgentActions(actions, currentAgentMap, conn);
 			
-			while (i < 70) {
+			while (i < 37) {
 				conn.do_timestep();
 				i++;
 				currentAgentMap = SimulationModelTraaS.updateCurrentAgentMap(completeAgentMap, currentAgentMap, conn);
+				SimulationModelTraaS.updateAgentsPhys(rn, currentAgentMap, conn);
 			}
 			
+			// Agent 2 is going maxSpeed, this means agent 1 was passed.
+			assertEquals(13.9, a2.getVelocity(),0.1);
+			assertEquals(agent1Speed, a1.getVelocity(),0.1);
 		}catch(Exception e){
 			e.printStackTrace();
 		}
@@ -118,7 +123,6 @@ public class PrepareAgentActionsTest {
 			HashMap<String, AgentAction> actions = new HashMap<String, AgentAction>();
 			
 			// Check for every action
-			// ChangeLane
 			AgentAction changeVelocityAction = AgentAction.ChangeVelocity;
 			actions.put(a1.agentID, changeVelocityAction);
 			
