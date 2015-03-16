fe870d703281c3220556ce4c59777b6e4c5d8a8a
Maxic <barend.poot@gmail.com>
2015-03-16 11:19:19 +0100

    :ocean: Added StayInLaneNorm

--- TrafficMAS/src/nl/uu/trafficmas/norm/StayLeftNormInstantiation.java
+++ TrafficMAS/src/nl/uu/trafficmas/norm/StayLeftNormInstantiation.java
@@ -1,10 +1,23 @@
 package nl.uu.trafficmas.norm;
 
+import nl.uu.trafficmas.simulationmodel.AgentData;
+
 public class StayLeftNormInstantiation extends NormInstantiation {
 
+	private AgentData agentData;
+	
 	public StayLeftNormInstantiation(NormScheme ns, String agentID) {
 		super(ns, agentID);
 		// TODO Auto-generated constructor stub
 	}
 
+	public void setGoal(AgentData agentData){
+		this.agentData = agentData;
+	}
+	
+	@Override
+	public AgentData goal() {
+		return this.agentData;
+	}
+	
 }
